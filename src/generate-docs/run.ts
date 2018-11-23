13a3faf007f856061136a4d87eba07ce5827ae5e
GregRos <life.gregr@gmail.com>
2018-11-23 15:09:19 +0200

    New publish script; Doc generation code

---
+++ src/generate-docs/run.ts
@@ -0,0 +1,24 @@
+import {Application} from "typedoc";
+import {WampusCustomizationPlugin} from "./typedoc-plugin";
+import * as execa from "execa";
+import globby = require("globby");
+
+async function run() {
+    let app = new Application({
+        module: "commonjs",
+        target: "es6",
+        plugin: [
+            "typedoc-plugin-external-module-name",
+            "typedoc-plugin-internal-external",
+            "typedoc-plugin-example-tag"
+        ]
+    });
+
+    let files = await globby(["./src/lib/**/*.ts", "./src/lib/*.ts"]);
+    await execa.shell("rm -rf docs/");
+    app.converter.addComponent("test", WampusCustomizationPlugin);
+
+    app.generateDocs(files, "docs")
+}
+
+run();
